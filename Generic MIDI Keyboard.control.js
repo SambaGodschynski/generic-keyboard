0e13c6cc5793a07a902074cf33f5c3f93cb5f282
Thomas Helzle <ThomasHelzle@users.noreply.github.com>
2014-12-31 19:08:24 +0100

    Added MMC Transport Control - if the controller sends it, the script will react.

--- Generic MIDI Keyboard.control.js
+++ Generic MIDI Keyboard.control.js
@@ -1,68 +1,55 @@
 
 loadAPI(1);
 
-var vendor = "Generic";
-var model = "MIDI Keyboard";
-var input = "MIDI Keyboard";
-
-var vendorPref;
-var modelPref;
-var lowCcPref;
-var highCcPref;
-
-host.defineController(vendor, model, "1.0", "6E55D132-1846-4C64-9F97-48041F2D9B96", "Bitwig");
+host.defineController("Generic", "MIDI Keyboard", "1.0", "6E55D132-1846-4C64-9F97-48041F2D9B96");
 host.defineMidiPorts(1, 0);
 
 var LOWEST_CC = 1;
 var HIGHEST_CC = 119;
 
-function init()
-{
-	 prefs = host.getPreferences();
-	 vendorPref = prefs.getStringSetting("Vendor: ", "Settings", 50, "Generic");
-	 vendorPref.addValueObserver(function(name){
-			println(name);
-			vendor = name;
-	 });
-	 modelPref = prefs.getStringSetting("Model: ", "Settings", 50, "MIDI Keyboard");
-	 modelPref.addValueObserver(function(name){
-			println(name);
-			model = name;
-			//host.restart;
-	 });
-	 lowCcPref = prefs.getNumberSetting("Lowest CC: ", "Settings", 1, 119, 1, "", 1);
-	 lowCcPref.addValueObserver(128, function(value){
-			println(value);
-			LOWEST_CC = value;
-	 });
-	 highCcPref = prefs.getNumberSetting("Highest CC: ", "Settings", 1, 119, 1, "", 119);
-	 highCcPref.addValueObserver(128, function(value){
-			println(value);
-			HIGHEST_CC = value;
-	 });
-
+function init() {
    host.getMidiInPort(0).setMidiCallback(onMidi);
-   generic = host.getMidiInPort(0).createNoteInput(model, "??????");
+   host.getMidiInPort(0).setSysexCallback(onSysex);
+   generic = host.getMidiInPort(0).createNoteInput("", "??????");
    generic.setShouldConsumeEvents(false);
+
+   transport = host.createTransport();
 
    // Make CCs 1-119 freely mappable
    userControls = host.createUserControls(HIGHEST_CC - LOWEST_CC + 1);
 
-   for(var i=LOWEST_CC; i<=HIGHEST_CC; i++)
-   {
+   for(var i=LOWEST_CC; i<=HIGHEST_CC; i++) {
       userControls.getControl(i - LOWEST_CC).setLabel("CC" + i);
    }
 }
 
-function onMidi(status, data1, data2)
-{
-   if (isChannelController(status))
-   {
-      if (data1 >= LOWEST_CC && data1 <= HIGHEST_CC)
-      {
+function onMidi(status, data1, data2) {
+   if (isChannelController(status)) {
+      if (data1 >= LOWEST_CC && data1 <= HIGHEST_CC) {
          var index = data1 - LOWEST_CC;
          userControls.getControl(index).set(data2, 128);
       }
+   }
+}
+
+function onSysex(data) {
+   // MMC Transport Controls:
+   switch (data) {
+      case "f07f7f0605f7":
+         transport.rewind();
+         break;
+      case "f07f7f0604f7":
+         transport.fastForward();
+         break;
+      case "f07f7f0601f7":
+         transport.stop();
+         break;
+      case "f07f7f0602f7":
+         transport.play();
+         break;
+      case "f07f7f0606f7":
+         transport.record();
+         break;
    }
 }
 
